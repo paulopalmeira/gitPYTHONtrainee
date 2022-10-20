225ea24fbc57d551523fd522da6de63a74f0cef2
Paulo Palmeira <108146896+paulopalmeira@users.noreply.github.com>
2022-10-20 15:38:51 -0300

    Update and rename 4opers.py to vetor.py

---
+++ vetor.py
@@ -0,0 +1,18 @@
+Lista_vazia = []
+
+Lista_inteiros = [2, 4, 6, 8, 10]
+
+Lista_reais = [9.0, 10.0, 8.5, 7.8]
+
+Lista_frutas = ["abacaxi", "pera", "uva", "abacate"]
+
+Lista_zerada = [0]*5
+
+Lista_strings = [""]*4
+
+print(Lista_vazia)
+print(Lista_inteiros)
+print(Lista_reais)
+print(Lista_frutas)
+print(Lista_zerada)
+print(Lista_strings)
